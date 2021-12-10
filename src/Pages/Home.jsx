b186b8041df1bb9ea67d9c8b61885657b7195b92
DiegoSE-FIME <ve06976@innovaccion.mx>
2021-12-09 23:39:31 -0600

    Home route initialized

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Home = () => {
+	return (
+		<div className="container">
+			<h1 className="text-center mt-5">User logged In 👋</h1>
+		</div>
+	);
+};
+
+export default Home;
