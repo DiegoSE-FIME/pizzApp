bdc131d5df9d773949b7f83bb68539335b59fb5b
DiegoSE-FIME <ve06976@innovaccion.mx>
2021-12-09 23:38:39 -0600

    Home route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import MainScreen from './Components/MainScreen';
 import { Routes, Route } from 'react-router-dom';
+
+import MainScreen from './Components/MainScreen';
+import Home from './Pages/Home';
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<MainScreen />} />
+				<Route path="/home" element={<Home />} />
 			</Routes>
 		</div>
 	);
