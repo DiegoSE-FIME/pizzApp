51d42b1dc3e94733f4400446da94e88bee82153c
DiegoSE-FIME <ve06976@innovaccion.mx>
2021-12-10 23:54:07 -0600

    User storage handling and log out function added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
-const Home = () => {
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import '../App.css';
+
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
+import Background from '../assets/imagebkg.png';
+import Pizza from '../assets/Pizza.png';
+import LoginIcon from '../assets/ic_password.png';
+
+import {
+	BackgroundImage,
+	SmallText,
+	StyledTitle,
+} from '../Components/styles/StyledComponents';
+
+function Home() {
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
+	}, []);
+
+	let navigate = useNavigate();
+
+	const logOut = () => {
+		navigate('/');
+		localStorage.clear();
+		console.log('salio');
+	};
+	const user = localStorage.getItem('user');
+
 	return (
 		<div className="container">
-			<h1 className="text-center mt-5">User logged In 👋</h1>
+			<div className="row">
+				<div className="col-sm wrapper">
+					<BackgroundImage src={Background}></BackgroundImage>
+					<img
+						src={Pizza}
+						alt="pizza"
+						className="pizza"
+						data-aos="fade-right"
+					/>
+				</div>
+
+				<div className="col-sm">
+					<div className="row">
+						<StyledTitle>Hola, {user}</StyledTitle>
+					</div>
+					<SmallText>Pizzerías</SmallText>
+					<div className="log-out-box">
+						<img src={LoginIcon} alt="log out" className="log-out-icon" />
+						<button
+							className="btn btn-link log-out-text"
+							type="button"
+							onClick={() => logOut()}>
+							Salir
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
