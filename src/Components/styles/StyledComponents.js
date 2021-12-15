e69f0b59851ffd5f4c6b08f742c13493436c2c53
DiegoSE-FIME <ve06976@innovaccion.mx>
2021-12-15 16:46:20 -0600

    responsive mobile design ui added

--- src/Components/styles/StyledComponents.js
+++ src/Components/styles/StyledComponents.js
@@ -1,14 +1,20 @@
 import styled from 'styled-components';
 
 export const BackgroundImage = styled.img`
-	width: 100%;
-	height: 100%;
+	width: 620px;
+	height: 711px;
+	position: absolute;
 `;
 
 export const InputForm = styled.input`
 	margin: 10px 0 10px 110px;
 	width: 500px;
 	height: 50px;
+
+	@media (max-width: 480px) {
+		margin: 10px 0 10px 10px;
+		width: 100%;
+	}
 `;
 
 export const StyledButton = styled.button`
@@ -22,6 +28,12 @@ export const StyledButton = styled.button`
 	margin-left: 110px;
 	font-size: ${(size) => size.theme.sizes.normalSize};
 	box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+
+	@media screen and (max-width: 480px) {
+		width: 100%;
+		position: relative;
+		margin-left: 10px;
+	}
 `;
 
 export const SmallText = styled.h5`
@@ -32,6 +44,9 @@ export const SmallText = styled.h5`
 	margin-left: 40px;
 	border-bottom: 3px solid ${(c) => c.theme.colors.yellow};
 	width: 90px;
+	@media screen and (max-width: 480px) {
+		margin-left: 40px;
+	}
 `;
 
 export const StyledTitle = styled.h1`
@@ -41,6 +56,7 @@ export const StyledTitle = styled.h1`
 	top: 40px;
 	left: 40px;
 	margin-bottom: 30px;
+	font-family: 'Marcellas', sans-serif;
 `;
 
 export const StyledCard = styled.div`
@@ -54,6 +70,9 @@ export const StyledCard = styled.div`
 	border-radius: 10px;
 	box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 	margin: 0 0 25px 35px;
+	@media screen and (max-width: 480px) {
+		margin: 0 0 25px 10px;
+	}
 `;
 
 export const StyledTitleSingleStore = styled(StyledTitle)`
@@ -61,10 +80,19 @@ export const StyledTitleSingleStore = styled(StyledTitle)`
 	width: 300px;
 	display: inline-block;
 	border-bottom: 3px solid ${(c) => c.theme.colors.yellow};
+	@media screen and (max-width: 480px) {
+		width: 300px;
+		position: relative;
+		top: 100px;
+		left: 10px;
+	}
 `;
 
 export const StoreDesc = styled.p`
 	font-size: 1.3rem;
 	color: ${(c) => c.theme.colors.black};
 	margin: 50px 0 0 40px;
+	@media screen and (max-width: 480px) {
+		margin: 100px 0 0 10px;
+	}
 `;
